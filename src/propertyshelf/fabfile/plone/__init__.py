ea7905e46cb5cc4bf09c157fbcf5bc6fd9ecf82d
Thomas Massmann <thomas@propertyshelf.com>
2013-12-12 12:29:58 -0500

    Manage frontend services using pfc.frontend.

--- src/propertyshelf/fabfile/plone/__init__.py
+++ src/propertyshelf/fabfile/plone/__init__.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 """Deploy and manage Propertyshelf Plone applications using Fabric."""
+
+import frontend
+
+
+frontend = frontend  # PyFlakes
+
+
+__all__ = [
+    'frontend',
+]
---
+++ src/propertyshelf/fabfile/plone/frontend.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Manage Plone frontend components like web server, load balancer, cache."""
+
+from fabric import api
+from propertyshelf.fabfile.common import frontend
+
+
+@api.task
+@api.roles('frontend')
+def restart():
+    """Restart the frontend components."""
+    restart_haproxy()
+    restart_varnish()
+    restart_nginx()
+
+
+@api.task
+@api.roles('frontend')
+def restart_nginx():
+    """Restart the NginX web server component."""
+    frontend.restart_nginx()
+
+
+@api.task
+@api.roles('frontend')
+def restart_varnish():
+    """Restart the Varnish caching proxy component."""
+    frontend.restart_varnish()
+
+
+@api.task
+@api.roles('frontend')
+def restart_haproxy():
+    """Restart the HA-Proxy load balancer component."""
+    frontend.restart_haproxy
